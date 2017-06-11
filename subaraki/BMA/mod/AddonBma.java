d5f7a186968e728a37d1091768d937b6cd91cfef
ArtixAllMighty <robbeenaxel@hotmail.com>
2017-06-11 12:34:36 +0200

    versioning !

--- subaraki/BMA/mod/AddonBma.java
+++ subaraki/BMA/mod/AddonBma.java
@@ -25,7 +25,7 @@ public class AddonBma {
 
 	public static final String MODID = "bma_addon";
 	public static final String NAME = "Berserker, Mage, Archer Addon for Rpg Inventory";
-	public static final String VERSION = "1.11.2 4.0.2.0";
+	public static final String VERSION = "1.11.2 4.0.3.0";
 
 	@SidedProxy(clientSide = "subaraki.BMA.handler.proxy.ClientProxy", serverSide = "subaraki.BMA.handler.proxy.ServerProxy")
 	public static ServerProxy proxy;
